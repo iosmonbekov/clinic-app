c2a36165818a6f99d7874b126641db8e6e4215a1
Islam Osmonbekov <islam.o@veles.finance>
2022-03-08 20:36:45 +0600

    connected db, and created required modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { DentistsModule } from './dentists/dentists.module';
+import { ReportsModule } from './reports/reports.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      database: 'clinic',
+      entities: [],
+      synchronize: true,
+    }),
+    DentistsModule,
+    ReportsModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(3000, () => {
+    console.log('Server started on PORT:', 3000);
+  });
 }
 
 bootstrap();
