691ace065f958a6eb1e1f7b9703680b7234f77d1
Islam Osmonbekov <islam.o@veles.finance>
2022-03-22 19:07:05 +0600

    added all validations

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -24,10 +24,29 @@ export class ReportsService {
     }
     throw new HttpException('Doctor have patient at this period', HttpStatus.BAD_REQUEST);
   }
-  editReport() {}
-  deleteReport() {}
 
   async validateDates({from, to}: ReportDto):Promise<boolean> {
+    const fromHour = new Date(from).getHours();
+    const toHour = new Date(to).getHours();
+
+    const fromDay = new Date(from).getDay();
+    const toDay = new Date(to).getDay();
+
+
+
+    if (fromHour > 12 && fromHour < 13 || toHour > 12 && toHour < 13) {
+      throw new HttpException('Lunch time between 12 - 13', HttpStatus.BAD_REQUEST);
+    }
+
+    if (fromDay > 5 || toDay > 5) {
+      throw new HttpException('Dentists don\' work on weekends', HttpStatus.BAD_REQUEST);
+    }
+
+    if (fromHour < 9 || fromHour > 17 || toHour < 9 || toHour > 17) {
+      console.log(fromHour, toHour)
+      throw new HttpException('Dentist works on period of 09:00 - 17:00', HttpStatus.BAD_REQUEST);
+    }
+
     const reports: ReportsEntity[] = await this.reportsRepository.query(
       `SELECT * FROM reports_entity 
              WHERE reports_entity.from < '${from}' AND reports_entity.to > '${from}' 
